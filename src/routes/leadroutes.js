55dc2ceb316f0e4bd0cb67f0a31d862d90081c21
Jean Carlos Soto Barbosa <jeansoto516@gmail.com>
2026-04-28 23:36:06 -0300

    fix: permitir voltar status lead

--- src/routes/leadroutes.js
+++ src/routes/leadroutes.js
@@ -1,26 +1,58 @@
 const express = require('express');
 const router = express.Router();
+
 const leadController = require('../controllers/leadController');
 const paymentController = require('../controllers/paymentController');
 const authMiddleware = require('../middlewares/authMiddleware');
-const checkSubscription = require('../middlewares/checkSubscription'); // TRIAL DE 14 DIAS PARA PAGAMENTO
+const checkSubscription = require('../middlewares/checkSubscription');
 
-//BASE PARA CRIAR AS DASHBOARDS E ESTATÍSTICAS
-router.get('/stats', authMiddleware, checkSubscription, leadController.getLeadsStats);
-router.get('/dashboard', authMiddleware, checkSubscription, leadController.getDashboard);
+// DASHBOARD
+router.get(
+    '/dashboard',
+    authMiddleware,
+    checkSubscription,
+    leadController.getDashboard
+);
 
-// criar lead 
-router.post('/', authMiddleware, checkSubscription, leadController.createLead);
+// CRIAR LEAD
+router.post(
+    '/',
+    authMiddleware,
+    checkSubscription,
+    leadController.createLead
+);
 
-// listar leads
-router.get('/', authMiddleware, checkSubscription, leadController.getLeads);
+//LISTAR LEADS
+router.get(
+    '/',
+    authMiddleware,
+    checkSubscription,
+    leadController.getLeads
+);
 
-// atualizar lead
-router.put('/:id', authMiddleware, checkSubscription, leadController.updateLead);
+//ATUALIZAR LEAD (STATUS PIPELINE)
+router.put(
+    '/:id',
+    authMiddleware,
+    checkSubscription,
+    leadController.updateLead
+);
 
-// criar pagamento
-router.post('/webhook', paymentController.webhook);
-router.post('/payment',paymentController.createPayment);
-router.post('/conduz/webhook', paymentController.webhook);
+//PAGAMENTOS
+router.post(
+    '/payment',
+    paymentController.createPayment
+);
+
+//WEBHOOKS
+router.post(
+    '/webhook',
+    paymentController.webhook
+);
+
+router.post(
+    '/conduz/webhook',
+    paymentController.webhook
+);
 
 module.exports = router;
