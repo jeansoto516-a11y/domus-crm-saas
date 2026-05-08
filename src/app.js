c60bedf64a6d1673c18c511565cfbcc80a7f291c
Jean Carlos Soto Barbosa <jeansoto516@gmail.com>
2026-05-08 10:06:04 -0300

    fix: render port binding

--- src/app.js
+++ src/app.js
@@ -1,40 +1,75 @@
 const express = require('express');
 const cors = require('cors');
+
 const pool = require('./config/db');
+
 const authRoutes = require('./routes/authRoutes');
 const authMiddleware = require('./middlewares/authMiddleware');
 const leadRoutes = require('./routes/leadroutes');
 
 const app = express();
 
-// middlewares
+
+// =========================
+// MIDDLEWARES
+// =========================
 app.use(cors());
 app.use(express.json());
 
-// rotas
+
+// =========================
+// ROTAS
+// =========================
 app.use('/auth', authRoutes);
 app.use('/leads', leadRoutes);
 
-// rota protegida de teste
+
+// =========================
+// ROTA TESTE
+// =========================
 app.get('/dashboard', authMiddleware, (req, res) => {
+
   res.json({
     message: 'Acesso permitido',
     user: req.user
   });
+
 });
 
-// PORTA DINÂMICA (IMPORTANTE PRO DEPLOY)
+
+// =========================
+// ROTA HOME
+// =========================
+app.get('/', (req, res) => {
+
+  res.send('DOMUS CRM API ONLINE 🚀');
+
+});
+
+
+// =========================
+// PORTA DINÂMICA
+// =========================
 const PORT = process.env.PORT || 3000;
 
-// conexão com banco + start servidor
+
+// =========================
+// START SERVER
+// =========================
 pool.connect()
   .then(() => {
-    console.log('Banco conectado ');
 
-    app.listen(PORT, () => {
+    console.log('Banco conectado');
+
+    app.listen(PORT, '0.0.0.0', () => {
+
       console.log(`Servidor rodando na porta ${PORT}`);
+
     });
+
   })
-  .catch(err => {
+  .catch((err) => {
+
     console.error('Erro ao conectar no banco', err);
+
   });
