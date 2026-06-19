afebc0bd42d47dbe96891867b95a2fbd9073b6f8
Jean Carlos Soto Barbosa <jeansoto516@gmail.com>
2026-06-19 18:41:31 -0300

    chore: ajustes iniciais para gestão de usuários

--- domus-backend/src/config/db.js
+++ domus-backend/src/config/db.js
@@ -1,16 +1,11 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-const databaseUrl = process.env.DATABASE_URL;
-
-if (!databaseUrl) {
-  console.error('DATABASE_URL nao definida.');
-  process.exit(1);
-}
-
 const pool = new Pool({
-  connectionString: databaseUrl,
-  ssl: process.env.DB_SSL === 'false' ? false : { rejectUnauthorized: false }
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
