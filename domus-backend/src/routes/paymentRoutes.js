675e7f59eec2f3ff90b46cfbd0537a812bd3315e
Jean Carlos Soto Barbosa <jeansoto516@gmail.com>
2026-07-22 18:59:00 -0300

    feat: refatoração das rotas der pagamento

--- domus-backend/src/routes/paymentRoutes.js
+++ domus-backend/src/routes/paymentRoutes.js
@@ -4,8 +4,9 @@ const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.post('/checkout', authMiddleware, paymentController.createPayment);
+router.post('/checkout/card', authMiddleware, paymentController.createCardSubscription);
+router.post('/checkout/pix', authMiddleware, paymentController.createPixCharge);
 router.post('/webhook', paymentController.webhook);
 router.post('/conduz/webhook', paymentController.webhook);
 
-module.exports = router;
+module.export = router;
