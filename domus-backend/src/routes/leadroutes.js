ee555fc504f1061b246d8b64e29a4b9f346a5a44
Jean Carlos Soto Barbosa <jeansoto516@gmail.com>
2026-08-01 15:12:28 -0300

    feat: criação da rota para exportar arquivos de leads

--- domus-backend/src/routes/leadroutes.js
+++ domus-backend/src/routes/leadroutes.js
@@ -6,6 +6,7 @@ const checkSubscription = require('../middlewares/checkSubscription');
 const router = express.Router();
 
 router.get('/dashboard', authMiddleware, checkSubscription, leadController.getDashboard);
+router.get('/export', authMiddleware, checkSubscription, leadController.exportLeads);
 router.post('/', authMiddleware, checkSubscription, leadController.createLead);
 router.get('/', authMiddleware, checkSubscription, leadController.getLeads);
 router.put('/:id', authMiddleware, checkSubscription, leadController.updateLead);
